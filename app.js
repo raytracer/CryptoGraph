33169cb4e10b944124da0833cd5a4a30be43652d
raytracer <swchris90@googlemail.com>
2014-11-24 22:34:12 +0100

    changed the session duration to 24h

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(session({ secret: 'keyboard cat', saveUninitialized: true,
-                 resave: true}));
+                 resave: true,
+                 cookie: {maxAge:86400000}}));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
